797a9ba152f9a99fe06e48f14ac4efa340898b4b
Edward Longman <eialongman@gmail.com>
2020-11-13 17:45:32 +0000

    Add canvas visualizer that plots the paths of OpportunisticRPL packets

---
+++ src/ORPLRouteCanvasVisualizer.cc
@@ -0,0 +1,43 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "ORPLRouteCanvasVisualizer.h"
+#include "OpportunisticRpl.h"
+
+Define_Module(ORPLRouteCanvasVisualizer);
+
+bool ORPLRouteCanvasVisualizer::isPathStart(cModule *module) const {
+    if (visualizer::NetworkRouteCanvasVisualizer::isPathStart(module) )
+        return true;
+    if (dynamic_cast<OpportunisticRpl* >(module) != nullptr)
+        return true;
+    return false;
+}
+
+bool ORPLRouteCanvasVisualizer::isPathEnd(cModule *module) const {
+    if (visualizer::NetworkRouteCanvasVisualizer::isPathEnd(module) )
+        return true;
+    if (dynamic_cast<OpportunisticRpl* >(module) != nullptr)
+        return true;
+    return false;
+}
+
+bool ORPLRouteCanvasVisualizer::isPathElement(cModule *module) const {
+    if (visualizer::NetworkRouteCanvasVisualizer::isPathElement(module) )
+        return true;
+    if (dynamic_cast<OpportunisticRpl* >(module) != nullptr)
+        return true;
+    return false;
+}
